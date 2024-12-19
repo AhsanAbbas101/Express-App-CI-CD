2b513d2ca5b0160d278d94d3464f62a35182e84a
Ahsan Abbas <ahsanabbas468@gmail.com>
2024-12-20 01:03:11 +0200

    docker compose added.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! - An Example Project For CI/CD - </h1>')
+  res.send('<h1>Hello World! - An Example Project For CI/CD - WatchTower Update!</h1>')
 })
 
 const PORT = 8080
